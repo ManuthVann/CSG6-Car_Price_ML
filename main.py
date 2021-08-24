3cd5c585cef58e75348d8054f6fa9d6222947e89
Votana.Srey <votana.srey@gmail.com>
2021-08-24 15:33:34 +0700

    Modified ML Model

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from flask import Flask, render_template, jsonify, request
 import pickle
 import pandas as pd
 import sklearn
+#from flask_mysql_connector import MySQL
+#from flask import MySQL
+#import yaml
 #import ML model
 app = Flask(__name__)
+
+#Config Database
+#db = yaml.load(open('database.yaml'))
+#app.config['MYSQL_HOST'] = db['mysql_host']
+#app.config['MYSQL_USER'] = db['mysql_user']
+#app.config['MYSQL_PASSWORD'] = db['mysql_password']
+#app.config['MYSQL_DB'] = db['mysql_db']
+
+#mysql = MySQL(app)
+
 model = pickle.load(open('loan_amount_prediction_easymoney.pickle', 'rb'))
 #set default route
 @app.route('/', methods=['GET'])
@@ -12,7 +25,30 @@ def home():
     return render_template(
         'custom_index.html'
     )
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#@app.route('/', methods=['POST', 'GET'])
+#def index():
+#    if request.method == 'POST':
+#        requester_detail = request.form
+#        installment = requester_detail['installment']
+#        term = requester_detail['term']
+#        borrower_month_income = requester_detail['borrower_month_income']
+#        borrower_length_experience = requester_detail['borrower_length_experience']
+#        home_ownership = requester_detail['home_ownership']
+#        address_state = requester_detail['address_state']
+#        #int_features = [int(x) for x in request.form.values()]
+#        #final_feature = [np.array(int_features)]
+#        #prediction = model.predict(final_feature)
+#        #round last 2 digit behind .
+#        #result = round(prediction[0], 2)
+#        prediction_result = requester_detail['prediction_result']
+#
+#        cur = mysql.connection.cursor()
+#        cur.execute("INSERT INTO loan_amount_database(installment,term,borrower_month_income,borrower_length_experience, home_ownership, address_state, prediction_result) Values  (%s, %s,%s, %s,%s, %s,%s);")
+#        mysql.connection.commit()
+#        cur.close()
+#    return render_template('custom_index.html')
+#    #return render_template('custom_index.html', prediction_text='Loan $ For Request-er $ {}'.format(result))
+
 @app.route('/predict', methods=['POST'])
 def predict():
     int_features = [int(x) for x in request.form.values()]
@@ -41,7 +77,6 @@ def predict_api():
 @app.route('/request_json', methods=["GET"])
 def request_json():
     data = request.get_json()
-
     installment = data['installment']
     term = data['term']
     borrower_month_income = data['borrower_month_income']
@@ -57,3 +92,4 @@ def request_json():
 #let it display debugging
 if __name__ == "__main__":
     app.run(debug=True)
+
