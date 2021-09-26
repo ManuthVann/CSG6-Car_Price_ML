88d82824b50eb52504ba0b138a3976ba27f4448f
Manuth_Vann <61499233+MANUTH-V@users.noreply.github.com>
2021-09-26 18:49:27 +0700

    There is an api connectd with web applicaiton to test our ml model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from flask import Flask, render_template, jsonify, request
+import joblib
 import pickle
 import pandas as pd
 import sklearn
@@ -7,6 +8,8 @@ import sklearn
 #from flask import MySQL
 #import yaml
 #import ML model
+
+
 app = Flask(__name__)
 
 #Config Database
@@ -18,76 +21,36 @@ app = Flask(__name__)
 
 #mysql = MySQL(app)
 
-model = pickle.load(open('loan_amount_prediction_easymoney.pickle', 'rb'))
+#model = pickle.load(open('car_price_prediction_xgboost.pickle', 'rb'))
+#model = joblib.load("car_price_prediction.joblib.dat")
+model_file = open("car_price_prediction_linear_regression.pickle",'rb')
+model = pickle.load(model_file)
+
 #set default route
 @app.route('/', methods=['GET'])
 def home():
     return render_template(
         'custom_index.html'
     )
-#@app.route('/', methods=['POST', 'GET'])
-#def index():
-#    if request.method == 'POST':
-#        requester_detail = request.form
-#        installment = requester_detail['installment']
-#        term = requester_detail['term']
-#        borrower_month_income = requester_detail['borrower_month_income']
-#        borrower_length_experience = requester_detail['borrower_length_experience']
-#        home_ownership = requester_detail['home_ownership']
-#        address_state = requester_detail['address_state']
-#        #int_features = [int(x) for x in request.form.values()]
-#        #final_feature = [np.array(int_features)]
-#        #prediction = model.predict(final_feature)
-#        #round last 2 digit behind .
-#        #result = round(prediction[0], 2)
-#        prediction_result = requester_detail['prediction_result']
-#
-#        cur = mysql.connection.cursor()
-#        cur.execute("INSERT INTO loan_amount_database(installment,term,borrower_month_income,borrower_length_experience, home_ownership, address_state, prediction_result) Values  (%s, %s,%s, %s,%s, %s,%s);")
-#        mysql.connection.commit()
-#        cur.close()
-#    return render_template('custom_index.html')
-#    #return render_template('custom_index.html', prediction_text='Loan $ For Request-er $ {}'.format(result))
 
-@app.route('/predict', methods=['POST'])
+
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
+
     int_features = [int(x) for x in request.form.values()]
     final_feature = [np.array(int_features)]
     prediction = model.predict(final_feature)
+
     # round last 2 digit behind .
     result = round(prediction[0], 2)
     #set a condition
     #if the result is greater than 0
     if result < 0:
-        return render_template('custom_index.html', prediction_text='Requester is NOT approved')
+        return render_template('custom_index.html', prediction_text='BOOM')
     # if the result is less than 0
     else:
-        return render_template('custom_index.html', prediction_text='Loan $ For Request-er $ {}'.format(result))
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-@app.route('/predict_api', methods=["GET","POST"])
-def predict_api():
-    #data = request.get_json(force=True)
-    data = request.json
-    query_df = pd.DataFrame(data)
-    prediction = model.predict(query_df)
-    # return flask.jsonify(**result)
-    return jsonify({"Loan $ Prediction Results: ": list(prediction)})
+        return render_template('custom_index.html', prediction_text='Car Price Prediction $ {}'.format(result))
 
-@app.route('/request_json', methods=["GET"])
-def request_json():
-    data = request.get_json()
-    installment = data['installment']
-    term = data['term']
-    borrower_month_income = data['borrower_month_income']
-    borrower_length_experience = data['borrower_length_experience']
-    home_ownership = data['home_ownership']
-    address_state = data['address_state']
-
-    prediction = model.predict(installment, term, borrower_month_income
-                               ,borrower_length_experience,home_ownership,
-                               address_state)
-    return jsonify({"Loan $ Prediction Results": prediction})
 
 #let it display debugging
 if __name__ == "__main__":
--- request.py
+++ request.py
@@ -1,11 +1,16 @@
 from flask import request, jsonify
 from flask import json
-url = 'http://localhost:5000/predict_api'
+url = 'http://localhost:5000/predict'
 r = request.post(url, json={
-    'installment':'borrower_length_experience',
-    'term':'term',
-    'borrower_month_income':'borrower_month_income',
-    'borrower_length_experience':'borrower_length_experience',
-    'home_ownership':'home_ownership',
-    'address_state':'address_state'
+    "year": "year",
+    "manufacturer": "manufacturer",
+    "cylinder": "cylinder",
+    "model": "model",
+    "fuel": "fuel",
+    "kms_driven": "kms_driven",
+    "status":"status",
+    "transmission":"transmission",
+    "drive_wheel":"drive_wheel",
+    "body_type":"body_type",
+    "colour":"colour"
 })
